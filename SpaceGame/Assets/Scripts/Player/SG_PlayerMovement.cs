d44122af02d319b2c70fbb2e272a7c7e33e7728a
masonjoyers <masonjoyers@gmail.com>
2015-04-05 00:09:01 -0400

    Added PlayerMovement
    Added the SG_PlayerMovement class, this controls how the player's
    fighter responses to input from the player.

---
+++ SpaceGame/Assets/Scripts/Player/SG_PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SG_PlayerMovement : MonoBehaviour {
+
+	// Setup the varibles for the player
+	public float maxSpeed = 1.0f;
+	public float currentSpeed = 0.6f;
+	public float boostSpeed = 0.2f;
+
+	public float maxYaw = 42.0f;
+	public float maxPitch = 42.0f;
+	public float maxRoll = 42.0;
+	public float currentYaw = 0.0f;
+	public float currentPitch = 0.0f;
+	public float currentRoll = 0.0f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// FixedUpdate is call for physics operations.
+	void FixedUpdate ()
+	{
+		// Store the input axes.
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+		
+
+	}
+}
